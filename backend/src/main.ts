6bd753f04c262b2a1cd089017abf02ca815e3b42
furoyal7 <fuadkediry@gmail.com>
2026-03-29 20:35:10 +0300

    fdhg

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -41,7 +41,8 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
-  await app.listen(process.env.PORT ?? 3000);
+  const port = process.env.PORT || 3000;
+await app.listen(port, '0.0.0.0');
   
   console.log(`🚀 Application is running on: http://localhost:${process.env.PORT ?? 3000}`);
   console.log(`📚 Swagger docs available at: http://localhost:${process.env.PORT ?? 3000}/api`);
